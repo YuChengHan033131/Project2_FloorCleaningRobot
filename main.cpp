0cb579629d57252beb9069a9f45799475bf902cb
游承翰 <chenghan03131@gapp.nthu.edu.tw>
2019-11-15 22:05:02 +0800

    robot->romaing&moveTo

--- main.cpp
+++ main.cpp
@@ -2,6 +2,14 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
+int abs(int num){
+    if(num<0){
+        return num*-1;
+    }else{
+        return num;
+    }
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 class Tile{
     private:
         bool cleaned;
@@ -68,6 +76,36 @@ class Tile{
         }
 };
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+class NodeB{
+    private:
+        int h;
+        int w;
+        bool visited=false;
+    public:
+        NodeB(int h,int w):h(h),w(w){}
+        NodeB(Tile *tile){
+            h=tile->_h();
+            w=tile->_w();
+        }
+        NodeB(){}
+        int _h(){
+            return h;
+        }
+        int _w(){
+            return w;
+        }
+        void setData(int h,int w){
+            this->h=h;
+            this->w=w;
+        }
+        void setVisited(bool visited){
+            this->visited=visited;
+        }
+        bool _visited(){
+            return visited;
+        }
+};
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 class Node{
     private:
         int h;
@@ -101,11 +139,21 @@ public:
     {
         head = new int[H * W];
     }
+    Field(Field *field) : H(field->H), W(field->W){
+        head = new int[H * W];
+    }
     int _data(int h, int w)
     {
         return head[h * W + w];
     }
+    int _data(int i)
+    {
+        return head[i];
+    }
     int _data(Node *node){
+        return head[node->_h() * W + node->_w()];
+    }
+    int _data(NodeB *node){
         return head[node->_h() * W + node->_w()];
     }
     int _H(){
@@ -186,10 +234,26 @@ class queue{
         }else if((_back+1)%_size!=_front){
             _back=(_back+1)%_size;
         }else{
-            cout << "queue is full! " ;
-            return;
+            increaseSize();
+            _back=(_back+1)%_size;
         }
         head[_back]=node;
+    }
+    void increaseSize(){
+        _size*=5/4+1;
+        T **newHead = new T* [_size];
+        for(int i=0;i<_size;i++){
+            if(size()!=0){
+                newHead[i]=front();
+                pop();
+            }else{
+                _front=0;
+                _back=i-1;
+                break;
+            }
+        }
+        delete(head);
+        head=newHead;
     }
     void pop(){
         if(_front==-1){
@@ -243,6 +307,13 @@ class queue{
         _back=-1;
         delete(_size);
     }
+    void destroy(){
+        while(_front!=-1){
+            T *temp=head[_front];
+            pop();
+            delete(temp);
+        }
+    }
 };
 queue <Node> q;
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -253,15 +324,163 @@ class Robot{
         int direction=0;//the direction robot heading.0=left,1=up,2=right,3=down
         int battery;
         int fullBattery;
+        int houseSize;
+        int cleanedBlock=0;
+        bool power=true;
+        Node *charger;
     public:
-        Robot(int initialH,int initialW,int battery):h(initialH),w(initialW),battery(battery),fullBattery(battery){}
+        Robot(int initialH,int initialW,int battery):h(initialH),w(initialW),battery(battery),fullBattery(battery){
+            charger=new Node(initialH,initialW);
+        }
         int _battery(){
             return battery;
         }
-        void findNearestDirtyBlock(){
-            cout << "findNearestDirtyBlock" << endl;
+        void setHouseSize(int houseSize){
+            this->houseSize=houseSize;
         }
-        void moveOneStepTo(Node *node){
+        bool isNotDone(){
+            if(cleanedBlock<houseSize){
+                return true;
+            }else if(cleanedBlock==houseSize){
+                return false;
+            }else{
+                cout << "error!cleanedBlock is largger than houseSize" << endl ;
+                return false;
+            }
+        }
+        void knockedOff(){//back to charger
+            moveTo(charger);
+            power=false;
+        }
+        bool powerIsOn(){
+            return power;
+        }
+        bool moveToNearestDirtyBlock(){//by BFS
+            //copy field
+            Field *tempfield=new Field(field);
+            for(int i=0;i<field->_H();i++){
+                for(int j=0;j<field->_W();j++){
+                    if(field->_data(i,j)!=1){
+                        tempfield->setData(i,j,0);
+                    }else{
+                        tempfield->setData(i,j,1);
+                    }
+                }
+            }
+            queue <Node> tempQ(houseSize);
+            Node *temp=new Node(h,w),*largest=NULL;
+            tempfield->setData(temp,2);//>1 visited 0=unvisted
+            tempQ.push(temp);
+            bool found=false;
+            while(tempQ.size()!=0){
+                temp=tempQ.front();
+                tempQ.pop();
+                //if find dirty block
+                if(field->_data(temp)>1){
+                    found=true;
+                    if(largest!=NULL){
+                        if(tempfield->_data(temp)>=tempfield->_data(largest)){//farthest block from start
+                            if(field->_data(largest)<field->_data(temp)){//farthest block from charger
+                                largest=temp;
+                            }
+                        }
+                    }else{
+                        largest=temp;
+                    }
+                    
+                    largest=temp;
+                }
+                //visit left
+                if(found==false&&tempfield->_data(temp->_h(),temp->_w()-1)==0){//means unvisited and not wall
+                    tempQ.push(new Node(temp->_h(),temp->_w()-1));
+                    tempfield->setData(temp->_h(),temp->_w()-1,tempfield->_data(temp)+1);
+                }
+                //visit up
+                if(found==false&&tempfield->_data(temp->_h()-1,temp->_w())==0){//means unvisited and not wall
+                    tempQ.push(new Node(temp->_h()-1,temp->_w()));
+                    tempfield->setData(temp->_h()-1,temp->_w(),tempfield->_data(temp)+1);
+                }
+                //visit right
+                if(found==false&&tempfield->_data(temp->_h(),temp->_w()+1)==0){//means unvisited and not wall
+                    tempQ.push(new Node(temp->_h(),temp->_w()+1));
+                    tempfield->setData(temp->_h(),temp->_w()+1,tempfield->_data(temp)+1);
+                }
+                //visit down
+                if(found==false&&tempfield->_data(temp->_h()+1,temp->_w())==0){//means unvisited and not wall
+                    tempQ.push(new Node(temp->_h()+1,temp->_w()));
+                    tempfield->setData(temp->_h()+1,temp->_w(),tempfield->_data(temp)+1);
+                }
+            }
+            if(found){
+                if(!moveTo(largest)){//no enough battery
+                    return false;
+                }
+                return true;
+            }else{
+                knockedOff();
+                return false;
+            }
+
+        }
+        /*void moveToNearestDirtyBlock(){//by BFS
+            //find nearest dirty block
+            NodeB *temp,*temp0,*temp1,*temp2,*temp3;
+            Node *largest=NULL;
+            queue <NodeB> tempQ(houseSize);
+            tempQ.push(new NodeB(h,w));
+            while(tempQ.size()!=0){
+                temp=tempQ.front();
+                temp->setVisited(true);
+                //chenck if charger is in wall
+
+                tempQ.pop();
+                //visit left
+                temp0=new NodeB(temp->_h(),temp->_w()-1);
+                if(field->_data(temp0)!=1&&temp0->_visited()==false){
+                    tempQ.push(temp0);
+                    temp0->setVisited(true);
+                }
+                //visit up
+                temp1=new NodeB(temp->_h()-1,temp->_w());
+                if(field->_data(temp1)!=1&&temp1->_visited()==false){
+                    tempQ.push(temp1);
+                    temp1->setVisited(true);
+                }
+                //visit right
+                temp2=new NodeB(temp->_h(),temp->_w()+1);
+                if(field->_data(temp2)!=1&&temp2->_visited()==false){
+                    tempQ.push(temp2);
+                    temp2->setVisited(true);
+                }
+                //visit down
+                temp3=new NodeB(temp->_h()+1,temp->_w());
+                if(field->_data(temp3)!=1&&temp3->_visited()==false){
+                    tempQ.push(temp3);
+                    temp3->setVisited(true);
+                }
+
+                //find farest dirty block 
+                NodeB *direction[4]={temp0,temp1,temp2,temp3};
+                for(int i=0;i<4;i++){
+                    if(field->_data(direction[i])>1){
+                        if(largest==NULL||field->_data(largest)<field->_data(direction[i])){
+                            largest=new Node(direction[i]->_h(),direction[i]->_w());
+                        }
+                    }
+                }
+                if(largest!=NULL){//found dirty block
+                    break;
+                }
+            }
+            //temp0=nearest dirty block
+            if(tempQ.size()!=0){
+                moveTo(largest);
+            }else{//no more dirty block
+                knockedOff();
+            }
+        }*/
+        bool moveOneStepTo(Node *node){
+            bool currentGridIsNotCleaned=false;
             int tempH=node->_h(),tempW=node->_w();
             if(field->_data(node)!=1&&
                ((tempH==h&&tempW==w-1)||//node is reachable and walkable
@@ -276,12 +495,27 @@ class Robot{
                     battery--;
                     if(field->_data(h,w)>1){//unclean
                         field->setData(h,w,field->_data(h,w)*-1);
+                        currentGridIsNotCleaned=true;
+                        cleanedBlock++;
                     }
                 }else{
                         battery=fullBattery;
                 }
             }else{
                 cout << "error!robot can't goes there." << endl ;
+            }
+            if(batteryNotEnough()){
+                //undo
+                battery++;
+                if(currentGridIsNotCleaned){
+                    field->setData(q.back(),field->_data(q.back())*-1);
+                }
+                q.pull();
+                h=q.back()->_h();
+                w=q.back()->_w();
+                return false;
+            }else{
+                return true;
             }
         }
         void moveOneStepAlongRight(){
@@ -297,6 +531,7 @@ class Robot{
                         battery--;
                         if(field->_data(h,w)>1){//unclean
                             field->setData(h,w,field->_data(h,w)*-1);
+                            cleanedBlock++;
                         }
                     }else{
                         battery=fullBattery;
@@ -306,9 +541,9 @@ class Robot{
                 direction=(direction+1)%4;
             }
             //no dirty block to walk
-            findNearestDirtyBlock();
+            moveToNearestDirtyBlock();
         }
-        queue <Node> *findShortestPath(Node *_from,Node *_to,int nodeNumbers){//by BFS 
+        queue <Node> *findShortestPath(Node *_from,Node *_to){//by BFS 
             //construct a matrix "similar" to field
            Tile **grid=new Tile* [field->_H()];
            for(int i=0;i<field->_H();i++){
@@ -328,7 +563,7 @@ class Robot{
                               ((grid[_to->_h()+1][_to->_w()]._data()==1)?0:1);
 
            //start finding
-           queue <Tile> q(nodeNumbers);
+           queue <Tile> q(houseSize);
            q.push(from);
            while(q.size()!=0){
                Tile *temp=q.front(),*temp2;
@@ -399,7 +634,7 @@ class Robot{
                 }
             }
             delete(&q);
-            queue <Node> *q1=new queue<Node>(nodeNumbers);
+            queue <Node> *q1=new queue<Node>(houseSize);
             //output path
             Tile *temp=&grid[_to->_h()][_to->_w()];
             while(temp!=from){
@@ -409,14 +644,69 @@ class Robot{
             delete(grid);
             return q1;
         }
-        void moveTo(Node *_to,int nodeNumbers){
-            queue <Node> *tempQ=findShortestPath(new Node(h,w),_to,nodeNumbers);
+        bool moveTo(Node *_to){
+            if(h==_to->_h()&&w==_to->_w()){//destination=start
+                return true;
+            }
+            queue <Node> *tempQ=findShortestPath(new Node(h,w),_to);
+            if(battery<tempQ->size()+abs(field->_data(_to))-1){//no enough battery to move to that place and go back
+                return false;
+            }
             while(tempQ->size()!=0){
                 Node *temp=tempQ->back();
                 tempQ->pull();
                 moveOneStepTo(temp);
             }
-
+            delete(tempQ);
+            return true;
+        }
+        /*int DistanceTo(Node *node){
+            queue <Node> *temp=findShortestPath(new Node(h,w),node);
+            int dis=temp->size();
+            temp->destroy();
+            return dis;
+        }*/
+        bool batteryNotEnough(){//if there are enought battery back to charger
+            int temp=field->_data(h,w);
+            if(temp<0){
+                temp*=-1;
+            }
+            return temp-1>battery;//since the field value is 1 larger than distance
+        }
+        void roaming(){//visit unclean & farthest(to charger) near by block
+            Node **direction=new Node*[4],*largest;
+            while(1){
+                if(h==charger->_h()&&w==charger->_w()){//means in the charger
+                    return;
+                }
+                int largestIndex=0;
+                direction[0]=new Node(h,w-1);
+                direction[1]=new Node(h-1,w);
+                direction[2]=new Node(h,w+1);
+                direction[3]=new Node(h+1,w);
+                largest=direction[0];
+                for(int i=1;i<4;i++){
+                    if(field->_data(largest)<field->_data(direction[i])){
+                        largest=direction[i];
+                        largestIndex=i;
+                    }
+                }
+                if(field->_data(largest)>1){//means it is not wall or clean block
+                   for(int i=0;i<4;i++){//delete usless node
+                        if(i!=largestIndex){
+                            delete(direction[i]);
+                        }
+                    }
+                    if(!moveOneStepTo(largest)){//means there are no enough battery to go back,so robot refuse to move that step
+                        return;
+                    }
+                    
+                }else{
+                    if(!moveToNearestDirtyBlock()){
+                        return;
+                    }
+                }
+            }
         }
         //test function 
         void jump(int h,int w){
@@ -437,6 +727,10 @@ class maxheap{
         for(int i=0;i<size;i++){
             head[i]=NULL;
         }
+    }
+    Node *_rootNode(){
+        heapify();
+        return head[0];
     }
     void insert(Node *node){
         head[size]=node;
@@ -537,7 +831,7 @@ int main()
             }
         }
     }
-    
+    bot->setHouseSize(WalkableBlockNumbers);
     //algorithm 1
     //counting all dsitance to charger(by BFS) and enter maxheap
     q.setSize(WalkableBlockNumbers);
@@ -577,22 +871,19 @@ int main()
     }
     field->setData(charger,-1);//recover R=-1
 
-    //move to afrest block in shortest path
-
-
-    //testing
-    field->print();
-    bot->moveTo(new Node(4,4),WalkableBlockNumbers);
-    bot->moveTo(new Node(1,6),WalkableBlockNumbers);
-    bot->moveTo(new Node(4,1),WalkableBlockNumbers);
-    field->print();
-
-    
+    field->print();//test
+    //cleaning
+    while(bot->powerIsOn()){
+        bot->moveTo(m._rootNode());//move to farthest block in shortest path
+        field->print();//test
+        bot->roaming();
+        field->print();//test
+        bot->moveTo(charger);
+        field->print();//test
+    }
 
     cout << endl ;
-    cout << q.size() ;
+    cout << q.size()-1 << endl ;
     q.printAll();
-    
-
-
+    return 0;
 }
